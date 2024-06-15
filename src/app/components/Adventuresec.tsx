aff2bfc754942ac98603d2db807fd593acab725b
Muhammad Shariq <muhammadshariq368@gmail.com>
2024-06-15 12:09:16 +0500

    change

--- src/app/components/Adventuresec.tsx
+++ src/app/components/Adventuresec.tsx
@@ -7,24 +7,24 @@ const Adventuresec = () => {
   return (
     <div className="mt-14 py-12">
     <div className="text-center mb-8">
-      <h2 className="text-5xl font-bold text-[#3190a8]">IT'S TIME TO START YOUR ADVENTURES</h2>
-      <p className="text-zinc-600">LOREM IPSUM DOLOR SIT AMET, ADIPISCING ELIT. VENIAM, FACILIS.</p>
+      <h2 className="text-5xl font-bold text-[#3190a8]">IT S TIME TO START YOUR ADVENTURES</h2>
+      <p className="text-zinc-600">LOREM IPSUM DOLOR SIT AMET ADIPISCING ELIT. VENIAM FACILIS.</p>
     </div>
     <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
       <div className="text-center">
         <Image src={mount} alt="Mountain Icon" className="mx-auto mb-4"/>
         <h3 className="text-lg font-bold text-orange-500">IN EVERY CONDITIONS</h3>
-        <p className="text-zinc-600 mt-5 px-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolorem harum aspernatur sapiente error, voluptas fuga, laudantium ullam magni fugit. Qui!</p>
+        <p className="text-zinc-600 mt-5 px-16">Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolorem harum aspernatur sapiente error voluptas fuga laudantium ullam magni fugit. Qui!</p>
       </div>
       <div className="text-center">
         <Image src={timer}  alt="Compass Icon" className="mx-auto mb-4"/>
         <h3 className="text-lg font-bold text-orange-500">PROFESSIONAL TEAM</h3>
-        <p className="text-zinc-600 mt-5 px-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolorem harum aspernatur sapiente error, voluptas fuga, laudantium ullam magni fugit. Qui!</p>
+        <p className="text-zinc-600 mt-5 px-16">Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolorem harum aspernatur sapiente error voluptas fuga laudantium ullam magni fugit. Qui!</p>
       </div>
       <div className="text-center">
         <Image src={bags} alt="Backpack Icon" className="mx-auto mb-4"/>
         <h3 className="text-lg font-bold text-orange-500">EXPERT HIKERS</h3>
-        <p className="text-zinc-600 mt-5 px-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolorem harum aspernatur sapiente error, voluptas fuga, laudantium ullam magni fugit. Qui!</p>
+        <p className="text-zinc-600 mt-5 px-16">Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolorem harum aspernatur sapiente error voluptas fuga laudantium ullam magni fugit. Qui!</p>
       </div>
     </div>
   </div>
